9a7b2dd6ea83614dd1530306c12ed9354400f3c8
Israel Aguilar <iaguilaror@gmail.com>
2019-03-31 22:04:41 -0600

    mk-tsv2bed-edit: implement bed formater

--- mkmodules/mk-tsv2bed-edit/tsv-editor.R
+++ mkmodules/mk-tsv2bed-edit/tsv-editor.R
@@ -5,7 +5,7 @@ options(scipen = 666)
 args <- commandArgs(trailingOnly = TRUE)
 
 ## For debug only
-# args[1] <- "test/data/sample_gnomad.genomes.coverage.summary.subsampled21.chunk0000.tsv"
+# args[1] <- "test/data/sample_gnomad.genomes.coverage.summary.subsampled21.chunk0000.edited.tmp"
 
 # args[2] <- "test/data/sample_gnomad.genomes.coverage.summary.subsampled21.chunk0000.edited.tsv"
 
@@ -14,6 +14,12 @@ data.df <- read.table(file = args[1], header = F, sep = "\t", stringsAsFactors =
 
 ## Round means
 data.df$V3 <- round(data.df$V3, digits = 2)
+
+## Repeat col 2 for bed end position
+data.df$V5 <- data.df$V2
+
+## Reorder columns
+data.df <- data.df[,c(1,2,5,3,4)]
 
 ## save data
 write.table(x = data.df, file = args[2], append = F, quote = F, sep = "\t", row.names = F, col.names = F)
